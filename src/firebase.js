4b85333ba5b6475ce9deb0fc1bf40a6fa7cea781
BrendanDB <BrendanDB@users.noreply.github.com>
2022-02-28 13:10:05 -0500

    Firebase credentials

--- src/firebase.js
+++ src/firebase.js
@@ -3,6 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   // À faire
+  apiKey: "AIzaSyAaV25zM5Ea2u-DyMlbDq7EzSMOlh4Lups",
+  authDomain: "exercice-firebase-2.firebaseapp.com",
+  projectId: "exercice-firebase-2",
+  storageBucket: "exercice-firebase-2.appspot.com",
+  messagingSenderId: "871773486119",
+  appId: "1:871773486119:web:007b1c104efd85bf1da202",
 };
 
 // Initialize Firebase and Firestore
